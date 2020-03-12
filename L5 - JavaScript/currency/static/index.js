1632e8da61160b40ac3c1ee155859933264bb601
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-03-13 01:29:37 +0800

    add lecture 7-8

--- L5 - JavaScript/currency/static/index.js
+++ L5 - JavaScript/currency/static/index.js
@@ -1,3 +1,5 @@
+/* jshint esversion: 6 */
+
 document.addEventListener('DOMContentLoaded', () => {
 
     document.querySelector('#form').onsubmit = () => {
@@ -8,13 +10,13 @@ document.addEventListener('DOMContentLoaded', () => {
         request.onload = () => {
             const data = JSON.parse(request.responseText);
             if (data.success) {
-                const contents = `1 EUR is equal to ${data.rate} ${currency}.`
+                const contents = `1 EUR is equal to ${data.rate} ${currency}.`;
                 document.querySelector('#result').innerHTML = contents;
             }
             else {
                 document.querySelector('#result').innerHTML = 'There was an error.';
             }
-        }
+        };
 
         const data = new FormData();
         data.append('currency', currency);
